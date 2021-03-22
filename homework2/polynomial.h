d959ae9203a9438ed172c3a56e26d721270a8c20
tatia2501 <79813229+tatia2501@users.noreply.github.com>
2021-03-23 02:36:10 +0300

    Update polynomial.h

--- homework2/polynomial.h
+++ homework2/polynomial.h
@@ -19,28 +19,28 @@ public:
 	friend bool operator==(const Polynomial& pol1, const Polynomial& pol2);
 	friend bool operator!=(const Polynomial& pol1, const Polynomial& pol2);
 
-	int& operator[](int index) const;
-	int& operator[](int index);
-
-	Polynomial zerocheck();
+	int& operator[](int num) const;
+	int& operator[](int num);
 
 	friend Polynomial operator+(const Polynomial& pol1, const Polynomial& pol2);
 	friend Polynomial operator-(const Polynomial& pol1, const Polynomial& pol2);
 	friend Polynomial operator-(const Polynomial& other);
-	friend Polynomial operator/(const Polynomial& other, int value);
-	friend Polynomial operator*(int value, const Polynomial& other);
-	friend Polynomial operator*(const Polynomial& other, int value);
+	friend Polynomial operator/(const Polynomial& other, int num);
+	friend Polynomial operator*(int num, const Polynomial& other);
+	friend Polynomial operator*(const Polynomial& other, int num);
 	friend Polynomial operator*(const Polynomial& pol1, const Polynomial& pol2);
 
-	friend Polynomial operator-=(const Polynomial& pol1, const Polynomial& pol2);
-	friend Polynomial operator+=(const Polynomial& pol1, const Polynomial& pol2);
-	friend Polynomial operator*=(const Polynomial& pol1, const Polynomial& pol2);
-	friend Polynomial operator/=(const Polynomial& other, int value);
+	double get(int num);
+	Polynomial zerocheck();
 
-	double get(int value);
+	Polynomial& operator-=(const Polynomial& other);
+	Polynomial& operator+=(const Polynomial& other);
+	Polynomial& operator*=(const Polynomial& other);
+	Polynomial& operator*=(int num);
+	Polynomial& operator/=(int num);
 
 	friend std::ostream& operator<<(std::ostream& stream, const Polynomial& other);
 	friend std::istream& operator>>(std::istream& stream, Polynomial& other);
 };
 
-#endif 
+#endif
